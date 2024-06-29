e131ba45cde3015c74fb88d1e771c1e44fd3aa6c
JATIN KUMAR GUPTA <103364821+jc8223@users.noreply.github.com>
2024-06-29 16:02:05 +0530

    Update language.config.js

--- configs/language.config.js
+++ configs/language.config.js
@@ -1,4 +1,4 @@
-const { CPP, C, PYTHON, JAVA, NODEJS, RUBY, PROMPTV1, PROMPTV2 } = require('../enums/supportedLanguages')
+const { CPP, C, PYTHON, JAVA, NODEJS, RUBY, PROMPTV1, PROMPTV2, REACTJS, ANGULAR, VUEJS } = require('../enums/supportedLanguages')
 const ONE_MB = 1024 // ulimit uses Kilobyte as base unit
 const ALLOWED_RAM = process.env.ALLOWED_RAM || 512
 
@@ -50,6 +50,27 @@ const LANGUAGES_CONFIG = {
     },
     [PROMPTV2]: {
         model: 'gpt-3.5-turbo-1106',
+    },
+    [REACTJS]: {
+        compile: 'npx create-react-app solution',
+        run: 'cd solution && npm start',
+        timeout: 30,
+        filename: 'solution',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [ANGULAR]: {
+        compile: 'ng new solution',
+        run: 'cd solution && ng serve',
+        timeout: 30,
+        filename: 'solution',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [VUEJS]: {
+        compile: 'vue create solution',
+        run: 'cd solution && npm run serve',
+        timeout: 30,
+        filename: 'solution',
+        memory: ALLOWED_RAM * ONE_MB,
     },
 }
 
